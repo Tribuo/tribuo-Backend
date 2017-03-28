6a8fc554c5ee01b271e28988d3112de92af4cac6
camiloAguado94 <camilo.aguado.94@gmail.com>
2017-03-27 22:47:30 -0500

    add static node

--- src/main/java/com/tribuo/backend/controllers/UsuariosController.java
+++ src/main/java/com/tribuo/backend/controllers/UsuariosController.java
@@ -6,16 +6,16 @@
 package com.tribuo.backend.controllers;
 
 import com.tribuo.backend.jpa.Usuarios;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
 import com.tribuo.backend.services.*;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
